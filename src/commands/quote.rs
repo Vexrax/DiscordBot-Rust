f42c05b39603acf29a7069a8876c1c7e67597e15
vexrax <joshuaf_111@live.com>
2023-03-25 11:55:51 -0400

    Add some code for getting all the quotes

--- src/commands/quote.rs
+++ src/commands/quote.rs
@@ -1,15 +1,40 @@
 use serenity::builder::CreateApplicationCommand;
 use serenity::model::prelude::interaction::application_command::CommandDataOption;
+use futures::stream::TryStreamExt;
+use serde::{Deserialize, Serialize};
+use mongodb::{bson::doc};
 use serenity::model::application::interaction::application_command::ApplicationCommandInteraction;
 use serenity::model::application::interaction::{Interaction, InteractionResponseType};
 use serenity::prelude::*;
 
+use crate::utils::mongo::get_mongo_client;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct Quote {
+    quote: String,
+    year: String,
+    author: String,
+    context: String,  
+}
+
 pub async fn run(_options: &[CommandDataOption], ctx: &Context, interaction: &Interaction, command: &ApplicationCommandInteraction) {
     todo!()
+
     // TODO quote
     // TODO quoteadd
     // TODO quotefrom
     // TODO backupQuotes
+}
+
+async fn get_all_quotes() -> mongodb::error::Result<Vec<Quote>> {
+    
+    let database = get_mongo_client().await?;
+
+    let typed_collection = database.collection::<Quote>("Quotes");
+
+    let cursor = typed_collection.find(None, None).await?;
+   
+    Ok(cursor.try_collect().await.unwrap_or_else(|_| vec![]))
 }
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
--- src/utils/mongo.rs
+++ src/utils/mongo.rs
@@ -5,7 +5,6 @@ use mongodb::{options::ClientOptions, Client, Database};
 
 pub async fn get_mongo_client() -> mongodb::error::Result<Database> {
     let mongo_pass = env::var("MONGOPASSWORD").expect("Expected mongopass in environment");
-    
     let mongo_connection_string = format!("mongodb+srv://Dueces:{}@cluster0-mzmgc.mongodb.net/test?retryWrites=true&w=majority", mongo_pass);
     let client_options = ClientOptions::parse(mongo_connection_string,).await?;
     let client = Client::with_options(client_options)?;
