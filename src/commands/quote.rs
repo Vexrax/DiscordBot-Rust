265403aff983ce5bd6cc47e49fbec8061884f7bc
vexrax <joshuaf_111@live.com>
2023-03-28 16:44:06 -0400

    Some more impl

--- src/commands/quote.rs
+++ src/commands/quote.rs
@@ -1,3 +1,4 @@
+use rand::seq::SliceRandom;
 use serenity::builder::CreateApplicationCommand;
 use futures::stream::TryStreamExt;
 use serde::{Deserialize, Serialize};
@@ -11,6 +12,7 @@ use serenity::model::prelude::interaction::application_command::{
     CommandDataOptionValue,
 };
 
+use crate::utils::discord_message::respond_to_interaction;
 use crate::utils::mongo::get_mongo_client;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -22,10 +24,30 @@ struct Quote {
 }
 
 pub async fn run(_options: &[CommandDataOption], ctx: &Context, interaction: &Interaction, command: &ApplicationCommandInteraction) {
+    get_random_quote(ctx, interaction, command).await;
     // TODO quote
     // TODO quoteadd
     // TODO quotefrom
     // TODO backupQuotes
+}
+
+async fn get_random_quote(ctx: &Context, interaction: &Interaction, command: &ApplicationCommandInteraction) {
+
+    respond_to_interaction(&ctx, &command, &"Some message here until i figure out why mongodb takes years to resp".to_string()).await;
+
+    let all_quotes: Vec<Quote>;
+
+    match get_all_quotes().await {
+        Ok(quote) => all_quotes = quote,
+        Err(err) => all_quotes = vec! []
+    }
+
+    let chosen_quote: &Quote = all_quotes.choose(&mut rand::thread_rng()).unwrap();
+    let _ = command.channel_id.send_message(&ctx.http, |m| {
+        m.embed(|e| e.title("")
+                                        .description(&format!("{} -{} {} {}", chosen_quote.quote, chosen_quote.author, chosen_quote.context, chosen_quote.year))                                    
+                                    )
+    }).await;
 }
 
 async fn get_all_quotes() -> mongodb::error::Result<Vec<Quote>> {
