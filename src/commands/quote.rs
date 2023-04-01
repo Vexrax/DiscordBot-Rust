3f90c573d6c20cc7c43637c24bd11ef1fe976e19
vexrax <joshuaf_111@live.com>
2023-04-01 11:11:28 -0400

    Some more quote stuff

--- src/commands/quote.rs
+++ src/commands/quote.rs
@@ -1,17 +1,19 @@
+use mongodb::bson::Document;
 use rand::seq::SliceRandom;
 use serenity::builder::CreateApplicationCommand;
 use futures::stream::TryStreamExt;
 use serde::{Deserialize, Serialize};
 use mongodb::{bson::doc};
 use serenity::model::application::interaction::application_command::ApplicationCommandInteraction;
-use serenity::model::application::interaction::{Interaction, InteractionResponseType};
+use serenity::model::application::interaction::{Interaction};
+use serenity::model::prelude::ChannelId;
 use serenity::prelude::*;
 use serenity::model::prelude::command::CommandOptionType;
 use serenity::model::prelude::interaction::application_command::{
     CommandDataOption,
     CommandDataOptionValue,
+    
 };
-
 use crate::utils::discord_message::respond_to_interaction;
 use crate::utils::mongo::get_mongo_client;
 
@@ -24,41 +26,88 @@ struct Quote {
 }
 
 pub async fn run(_options: &[CommandDataOption], ctx: &Context, interaction: &Interaction, command: &ApplicationCommandInteraction) {
-    get_random_quote(ctx, interaction, command).await;
-    // TODO quote
+
+    if _options.get(0).is_none() {
+        get_random_quote(ctx, command).await;
+        return;
+    }
+
+    let option = _options.get(0)
+                                                  .expect("Expected auther to be specified")
+                                                  .resolved
+                                                  .as_ref()
+                                                  .expect("Expected string object");
+
+    if let CommandDataOptionValue::String(name) = option {
+        get_quote_from(name, ctx, command).await;
+    } 
+    
     // TODO quoteadd
-    // TODO quotefrom
     // TODO backupQuotes
 }
 
-async fn get_random_quote(ctx: &Context, interaction: &Interaction, command: &ApplicationCommandInteraction) {
-
-    respond_to_interaction(&ctx, &command, &"Some message here until i figure out why mongodb takes years to resp".to_string()).await;
+async fn get_quote_from(name: &String, ctx: &Context, command: &ApplicationCommandInteraction) {
+    respond_to_interaction(&ctx, &command, &format!("getting a quote from {}. . .", name).to_string()).await;
 
     let all_quotes: Vec<Quote>;
 
-    match get_all_quotes().await {
+    match get_quotes(doc! { "author": capitalize(name) }).await {
         Ok(quote) => all_quotes = quote,
         Err(err) => all_quotes = vec! []
     }
 
-    let chosen_quote: &Quote = all_quotes.choose(&mut rand::thread_rng()).unwrap();
-    let _ = command.channel_id.send_message(&ctx.http, |m| {
+    send_quote_in_channel(ctx, &command.channel_id, all_quotes).await
+}
+
+async fn get_random_quote(ctx: &Context, command: &ApplicationCommandInteraction) {
+
+    respond_to_interaction(&ctx, &command, &"Getting a random quote. . .".to_string()).await;
+
+    let all_quotes: Vec<Quote>;
+
+    match get_quotes(doc! {  }).await {
+        Ok(quote) => all_quotes = quote,
+        Err(err) => all_quotes = vec! []
+    }
+
+    send_quote_in_channel(ctx, &command.channel_id, all_quotes).await
+}
+
+
+async fn send_quote_in_channel(ctx: &Context, channel_id: &ChannelId, quotes: Vec<Quote>) {
+    let optional_quote: Option<&Quote> = quotes.choose(&mut rand::thread_rng());
+
+    if optional_quote.is_none() {
+        let _ = channel_id.send_message(&ctx.http, |m| {
+            m.embed(|e| e.title("ERROR")
+                                            .description(&format!("Couldnt find a quote!"))                                    
+                                        )
+        }).await;
+        return;
+    }
+
+    let chosen_quote = optional_quote.unwrap();
+
+    let _ = channel_id.send_message(&ctx.http, |m| {
         m.embed(|e| e.title("")
                                         .description(&format!("{} -{} {} {}", chosen_quote.quote, chosen_quote.author, chosen_quote.context, chosen_quote.year))                                    
                                     )
     }).await;
 }
 
-async fn get_all_quotes() -> mongodb::error::Result<Vec<Quote>> {
-    
+async fn get_quotes(filter: Document) -> mongodb::error::Result<Vec<Quote>> {
     let database = get_mongo_client().await?;
-
     let typed_collection = database.collection::<Quote>("Quotes");
-
-    let cursor = typed_collection.find(None, None).await?;
-   
+    let cursor = typed_collection.find(filter, None).await?;
     Ok(cursor.try_collect().await.unwrap_or_else(|_| vec![]))
+}
+
+pub fn capitalize(s: &str) -> String {
+    let mut c = s.chars();
+    match c.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+    }
 }
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
