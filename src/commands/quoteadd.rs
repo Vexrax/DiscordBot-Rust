a8775f22b2741bc84ba0c47751912e39f5163403
vexrax <joshuaf_111@live.com>
2023-11-29 21:18:11 -0500

    Fix quoteadd

--- src/commands/quoteadd.rs
+++ src/commands/quoteadd.rs
@@ -12,7 +12,6 @@ use crate::utils::discord_message::respond_to_interaction;
 use crate::utils::mongo::get_mongo_client;
 use crate::utils::string_utils::capitalize;
 
-
 // TODO commonize this with the one in quote 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 struct Quote {
@@ -22,62 +21,62 @@ struct Quote {
     context: String,  
 }
 
-pub async fn run(_options: &[ResolvedOption<'_>], ctx: &Context, command: &CommandInteraction) {
-    // return "Reimpliment this later".to_string();
+pub async fn run(options: &[ResolvedOption<'_>], ctx: &Context, command: &CommandInteraction) {
+    let quote: String;
+    let author: String;
+    let year: String;
+    if let Some(ResolvedOption { value: ResolvedValue::String(quote_option), .. }) = options.get(0) {
+        quote = quote_option.to_string();
+    } else {
+        respond_to_interaction(&ctx, &command, &format!("Expected quote to be specified").to_string()).await;
+        return;
+    }
+
+    if let Some(ResolvedOption { value: ResolvedValue::String(author_option), .. }) = options.get(1) {
+        author = author_option.to_string();
+    } else {
+        respond_to_interaction(&ctx, &command, &format!("Expected author to be specified").to_string()).await;
+        return;
+    }
+
+    if let Some(ResolvedOption { value: ResolvedValue::String(year_option), .. }) = options.get(2) {
+        year = year_option.to_string();
+    } else {
+        respond_to_interaction(&ctx, &command, &format!("Expected year to be specified").to_string()).await;
+        return;
+    }
+
+
+    if let Some(member) = &command.member {
+        // Vexrax userId
+        if member.user.id != 188313190214533120 {
+            respond_to_interaction(&ctx, &command, &format!("No perms").to_string()).await;
+            return;
+        }
+    }
+
+    let quote_to_add = Quote {
+        quote: capitalize(&quote),
+        author: author.to_string(),
+        year: year.to_string(),
+        context: "".to_string()
+    };
+
+    respond_to_interaction(&ctx, &command, &format!("Adding the quote: `{}` -{} {}", quote_to_add.quote, quote_to_add.author, quote_to_add.year).to_string()).await;
+
+    let database_result = get_mongo_client().await;
+
+    match database_result {
+        Ok(db) => add_quote_to_collection(db.collection::<Quote>("Quotes"), quote_to_add).await,
+        Err(err) => {
+          eprintln!("Error: something went wrong when trying to add a quote to the DB: {}", err);  
+        }
+    }
 }
 
-// pub async fn run(_options: &[CommandDataOption], ctx: &Context, _interaction: &Interaction, command: &ApplicationCommandInteraction) {
-
-//     let quote_option = _options .get(0)
-//                                 .expect("Expected quote to be specified")
-//                                 .resolved
-//                                 .as_ref()
-//                                 .expect("Expected string object");
-//     let author_option = _options.get(1)
-//                                 .expect("Expected author to be specified")
-//                                 .resolved
-//                                 .as_ref()
-//                                 .expect("Expected string object");
-//     let year_option = _options.get(2)
-//                                 .expect("Expected year to be specified")
-//                                 .resolved
-//                                 .as_ref()
-//                                 .expect("Expected string object");
-
-//     if let Some(member) = &command.member {
-//         // Vexrax userId
-//         if member.user.id != 188313190214533120 {
-//             respond_to_interaction(&ctx, &command, &format!("No perms").to_string()).await;
-//             return;
-//         }
-//     }
-
-//     let quote = if let CommandDataOptionValue::String(quote) = quote_option { quote } else { todo!() };
-//     let author = if let CommandDataOptionValue::String(author) = author_option { author } else { todo!() };
-//     let year = if let CommandDataOptionValue::String(year) = year_option { year } else { todo!() };
-
-//     let quote_to_add = Quote {
-//         quote: capitalize(quote),
-//         author: author.to_string(),
-//         year: year.to_string(),
-//         context: "".to_string()
-//     };
-
-//     respond_to_interaction(&ctx, &command, &format!("Adding the quote: `{}` -{} {}", quote_to_add.quote, quote_to_add.author, quote_to_add.year).to_string()).await;
-
-//     let database_result = get_mongo_client().await;
-
-//     match database_result {
-//         Ok(db) => add_quote_to_collection(db.collection::<Quote>("Quotes"), quote_to_add).await,
-//         Err(err) => {
-//           eprintln!("Error: something went wrong when trying to add a quote to the DB: {}", err);  
-//         }
-//     }
-// }
-
-// async fn add_quote_to_collection(collection: Collection<Quote>, quote_to_add: Quote){
-//     collection.insert_one(quote_to_add, None).await.ok();
-// }
+async fn add_quote_to_collection(collection: Collection<Quote>, quote_to_add: Quote){
+    collection.insert_one(quote_to_add, None).await.ok();
+}
 
 pub fn register() -> CreateCommand {
     CreateCommand::new("quoteadd")
