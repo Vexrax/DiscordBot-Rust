8d86d9cd421e79f9b9cda392fa21ac7d46b54111
Vexrax <joshuaf_111@live.com>
2023-12-02 19:30:52 -0500

    Polish a bit

--- src/commands/gamestatus.rs
+++ src/commands/gamestatus.rs
@@ -1,13 +1,14 @@
 use std::env;
+use std::fmt::format;
 
 use riven::models::league_v4::LeagueEntry;
-use riven::models::spectator_v4::CurrentGameParticipant;
-use serenity::all::{CommandInteraction, CommandDataOptionValue, ResolvedValue, CommandOptionType};
-use serenity::builder::{CreateCommand, CreateCommandOption, CreateEmbed, CreateMessage};
+use serenity::all::{CommandInteraction};
+use serenity::builder::{CreateCommand, CreateEmbed, CreateMessage};
 use serenity::client::Context;
 use serenity::model::application::ResolvedOption;
 use riven::RiotApi;
 use riven::consts::{PlatformRoute, QueueType, Team, Champion};
+use riven::models::summoner_v4::Summoner;
 
 use crate::utils::discord_message::respond_to_interaction;
 
@@ -33,7 +34,7 @@ pub async fn run(_options: &[ResolvedOption<'_>], ctx: &Context, command: &Comma
 
     let riot_account = riot_api.account_v1().get_by_riot_id(riven::consts::RegionalRoute::AMERICAS, game_name, tagline)
     .await
-    .expect("Get Summeron Failed")
+    .expect("Get Summoner Failed")
     .expect("There is no summoner with that name");
 
     let riot_summoner = riot_api.summoner_v4().get_by_puuid(PlatformRoute::NA1, &riot_account.puuid).await
@@ -49,27 +50,38 @@ pub async fn run(_options: &[ResolvedOption<'_>], ctx: &Context, command: &Comma
         match_players.push(MatchPlayer { rank: rank, champion_id: participant.champion_id, profile_icon_id: participant.profile_icon_id, team_id: participant.team_id, summoner_name: participant.summoner_name })
     }
 
-    let match_embed = build_embed(game_name.to_string(), tagline.to_string(), match_players);
+    let match_embed = build_embed(game_name.to_string(), tagline.to_string(), riot_summoner, match_players);
     let _ = command.channel_id.send_message(&ctx.http,CreateMessage::new().tts(false).embed(match_embed)).await;
 }
 
-fn build_embed(main_player_game_name: String, main_player_tagline: String, match_players: Vec<MatchPlayer>) -> CreateEmbed {
+fn build_embed(main_player_game_name: String, main_player_tagline: String, main_player_riot_summoner: Summoner, match_players: Vec<MatchPlayer>) -> CreateEmbed {
     // TODO build cool embed!
-    let mut fields: Vec<(String, String, bool)> = vec![];
+    let mut blue_fields: Vec<(String, String, bool)> = vec![];
+    let mut red_fields: Vec<(String, String, bool)> = vec![];
+
     for match_player in match_players {
         let display_rank = match_player.rank.as_ref()
             .map(|val| format!("{:?} {:?}", val.tier.unwrap(), val.rank.unwrap())) // TODO might need to handle this better
             .unwrap_or_else(|| "UNRANKED".to_string());
-        let title = format!("{} ({:?})",match_player.summoner_name, match_player.champion_id.identifier().unwrap_or_else(|| "Unknown Champ?"));
+        let title = format!("{} ({})",match_player.summoner_name, match_player.champion_id.identifier().unwrap_or_else(|| "Unknown Champ?"));
 
-        fields.push((title, display_rank, false));
+        if (match_player.team_id == Team::BLUE) {
+            blue_fields.push((title, display_rank, true));
+        } else if (match_player.team_id == Team::RED) {
+            red_fields.push((title, display_rank, true));
+        }
     }
 
+    // TODO move this out
+    let ddragon_base: &str = "http://ddragon.leagueoflegends.com/cdn/10.25.1";
+    let ddragon_base_icon: String = format!("{}/img/profileicon/", ddragon_base);
 
     let embed = CreateEmbed::new()
         .title(format!("{}#{}'s Game", main_player_game_name, main_player_tagline))
         .description(&format!("Some Description"))
-        .fields(fields.into_iter());
+        .thumbnail(format!("{}{}.png", ddragon_base_icon, main_player_riot_summoner.profile_icon_id))
+        .fields(blue_fields.into_iter())
+        .fields(red_fields.into_iter());
     return embed;
 }
 
