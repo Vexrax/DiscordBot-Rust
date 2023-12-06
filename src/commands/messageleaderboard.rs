cbd4971501700af54a747feba6c26574a2ccc0bc
Vexrax <joshuaf_111@live.com>
2023-12-05 20:40:22 -0500

    More impl for message leaderboards

--- src/commands/messageleaderboard.rs
+++ src/commands/messageleaderboard.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use futures::StreamExt;
 use serenity::all::{ChannelId, CommandInteraction, GetMessages, User};
 use serenity::builder::{CreateCommand};
 use serenity::client::Context;
@@ -7,23 +8,42 @@ use serenity::model::application::ResolvedOption;
 use crate::utils::discord_message::respond_to_interaction;
 
 pub async fn run(_options: &[ResolvedOption<'_>], ctx: &Context, command: &CommandInteraction) {
+    respond_to_interaction(ctx, command, &format!("Calculating Message Counts").to_string()).await;
+
     let guild = command.guild_id.unwrap();
     let all_channels = guild.channels(&ctx.http).await.unwrap();
 
     let mut message_counts: HashMap<User, u32> = HashMap::new();
 
     for channels in all_channels {
-        message_counts.extend(get_all_messages_in_channel(channels.0, ctx));
+        message_counts = message_counts.into_iter().chain(get_all_messages_in_channel(channels.0, ctx).await).collect();
     }
 
-    // TODO create embed of top 10
-
-    respond_to_interaction(ctx, command, &format!("Skynet V3 (Rust Version)").to_string()).await;
+    for x in message_counts {
+        println!("{} - {}", x.0.name, x.1);
+    }
 }
 
+// TODO this function is super inefficent, need to find a faster way to do this
 pub async fn get_all_messages_in_channel(channel_id: ChannelId, ctx: &Context) -> HashMap<User, u32> {
-    // channel_id.messages(&ctx.http, GetMessages::new().limit(255));
-    todo!()
+
+    let mut message_counts: HashMap<User, u32> = HashMap::new();
+
+    let mut before = 255;
+    let mut after = 1;
+    let mut messages_in_channel = channel_id.messages(&ctx.http, GetMessages::new().before(before).after(after)).await.unwrap();
+
+    while messages_in_channel.len() > 0 {
+        for message in messages_in_channel.clone() {
+            let x = message_counts.entry(message.author.clone()).or_insert(0);
+            let y  = x.clone();
+            message_counts.insert(message.author, y + 1);
+        }
+        before += 255;
+        after += 255;
+        messages_in_channel = channel_id.messages(&ctx.http,  GetMessages::new().before(before).after(after)).await.unwrap();
+    }
+    return HashMap::new();
 }
 
 pub fn register() -> CreateCommand {
