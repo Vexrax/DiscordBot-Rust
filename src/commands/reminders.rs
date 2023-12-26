2ea843ca112182f21fb053dfafefec2c3118ef92
Vexrax <joshuaf_111@live.com>
2023-12-26 15:47:42 -0500

    More implementation of reminders, update mongo account to use role defined user

--- src/commands/reminders.rs
+++ src/commands/reminders.rs
@@ -1,13 +1,19 @@
-use serenity::all::{CommandInteraction, CommandOptionType, ResolvedValue};
+use serenity::all::{ChannelId, CommandInteraction, CommandOptionType, GuildId, ResolvedValue};
 use serenity::builder::{CreateCommand, CreateCommandOption};
 use serenity::client::Context;
 use serenity::model::application::ResolvedOption;
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::str::FromStr;
+use mongodb::Collection;
+use serde::{Deserialize, Serialize};
 use crate::utils::discord_message::respond_to_interaction;
+use crate::utils::mongo::get_mongo_client;
 
+#[derive(Clone, Debug, Deserialize, Serialize)]
 struct Reminder {
     reminder: String,
+    guild_id: GuildId,
+    channel_id: ChannelId,
     time: u128
 }
 
@@ -38,6 +44,11 @@ pub async fn run(options: &[ResolvedOption<'_>], ctx: &Context, command: &Comman
     let reminder: String;
     let amount: i64;
     let unit: TimeUnit;
+    let guild_id: GuildId = command.guild_id.expect("Expected this message to be in a guild");
+    let channel_id: ChannelId = command.channel_id;
+
+    println!("HEEEEEERRRE");
+
     if let Some(ResolvedOption { value: ResolvedValue::String(reminder_option), .. }) = options.get(0) {
         reminder = reminder_option.to_string();
     } else {
@@ -65,14 +76,28 @@ pub async fn run(options: &[ResolvedOption<'_>], ctx: &Context, command: &Comman
         .expect("Time went backwards");
 
     let time_in_future_millis = i128::from(amount) * get_millisecond_conversion_factor(unit);
-    let timestamp_to_remind_at = current_time_millia.as_millis().wrapping_add_signed(time_in_future_millis);
+    let timestamp_to_remind_at_millis = current_time_millia.as_millis().wrapping_add_signed(time_in_future_millis);
 
     let reminder = Reminder {
         reminder,
-        time: timestamp_to_remind_at,
+        guild_id,
+        channel_id,
+        time: timestamp_to_remind_at_millis,
     };
 
-    // TODO add to DB
+    // TODO?
+    // Maybe have a limit of 10 reminders per person?
+
+    println!("{} {} {} {}", reminder.reminder, reminder.time, reminder.channel_id, reminder.guild_id);
+
+    let database_result = get_mongo_client().await;
+    match database_result {
+        // TODO figure out why this doesnt insert correctly
+        Ok(db) => add_reminder_to_collection(db.collection::<Reminder>("Reminders"), reminder).await,
+        Err(err) => {
+            eprintln!("Error: something went wrong when trying to add a reminder to the DB: {}", err);
+        }
+    }
 }
 
 pub fn register() -> CreateCommand {
@@ -90,6 +115,18 @@ pub fn register() -> CreateCommand {
             CreateCommandOption::new(CommandOptionType::String, "unit", "Unit Of Measurement")
                 .required(true),
         )
+}
+
+pub fn check_for_reminders() {
+    // Pull the active reminders
+    // Check if any are due
+    // Make the call to the channel
+    // Delete reminder from DB
+    todo!()
+}
+
+async fn add_reminder_to_collection(collection: Collection<Reminder>, reminder:  Reminder) {
+    collection.insert_one(reminder, None).await.ok();
 }
 
 fn get_millisecond_conversion_factor(unit_from_user: TimeUnit) -> i128 {
