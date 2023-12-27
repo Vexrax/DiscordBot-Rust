2319d2c5527086b7eb4aa9113bb26eea43cef153
Vexrax <joshuaf_111@live.com>
2023-12-26 23:11:25 -0500

    Making saving to DB work

--- src/commands/reminders.rs
+++ src/commands/reminders.rs
@@ -1,20 +1,23 @@
+use std::ops::Add;
 use serenity::all::{ChannelId, CommandInteraction, CommandOptionType, GuildId, ResolvedValue};
 use serenity::builder::{CreateCommand, CreateCommandOption};
 use serenity::client::Context;
 use serenity::model::application::ResolvedOption;
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::{SystemTime, UNIX_EPOCH, Duration};
 use std::str::FromStr;
 use mongodb::Collection;
 use serde::{Deserialize, Serialize};
+use chrono::prelude::DateTime;
+use chrono::{Local, Utc};
 use crate::utils::discord_message::respond_to_interaction;
 use crate::utils::mongo::get_mongo_client;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 struct Reminder {
     reminder: String,
-    guild_id: GuildId,
-    channel_id: ChannelId,
-    time: u128
+    guild_id: u64,
+    channel_id: u64,
+    time: u64
 }
 
 #[derive(Clone)]
@@ -47,8 +50,6 @@ pub async fn run(options: &[ResolvedOption<'_>], ctx: &Context, command: &Comman
     let guild_id: GuildId = command.guild_id.expect("Expected this message to be in a guild");
     let channel_id: ChannelId = command.channel_id;
 
-    println!("HEEEEEERRRE");
-
     if let Some(ResolvedOption { value: ResolvedValue::String(reminder_option), .. }) = options.get(0) {
         reminder = reminder_option.to_string();
     } else {
@@ -70,34 +71,38 @@ pub async fn run(options: &[ResolvedOption<'_>], ctx: &Context, command: &Comman
         return;
     }
 
-
-    let current_time_millia = SystemTime::now()
+    let current_time_seconds = SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .expect("Time went backwards");
 
-    let time_in_future_millis = i128::from(amount) * get_millisecond_conversion_factor(unit);
-    let timestamp_to_remind_at_millis = current_time_millia.as_millis().wrapping_add_signed(time_in_future_millis);
+    let time_in_future_seconds = i64::from(amount) * get_second_conversion_factor(unit);
+    let timestamp_to_remind_at_seconds = current_time_seconds.as_secs().wrapping_add_signed(time_in_future_seconds);
 
     let reminder = Reminder {
         reminder,
-        guild_id,
-        channel_id,
-        time: timestamp_to_remind_at_millis,
+        guild_id: guild_id.get(),
+        channel_id: channel_id.get(),
+        time: timestamp_to_remind_at_seconds,
     };
 
-    // TODO?
-    // Maybe have a limit of 10 reminders per person?
+    // TODO: Maybe have a limit of 10 reminders per person?
 
-    println!("{} {} {} {}", reminder.reminder, reminder.time, reminder.channel_id, reminder.guild_id);
+    // Format to human readable
+    let timestamp_in_future = SystemTime::now().checked_add(Duration::from_secs(time_in_future_seconds as u64)).expect("Expected the time to be a u64");
+    let datetime = DateTime::<Local>::from(timestamp_in_future); // using locale for now, should find a way to use EST in the future
+
+    // TODO use month names
+    respond_to_interaction(&ctx, &command, &format!("I will remind you about: '{}' on {} EST", &reminder.reminder, datetime.format("%Y-%m-%d %H:%M").to_string())).await;
 
     let database_result = get_mongo_client().await;
+
     match database_result {
-        // TODO figure out why this doesnt insert correctly
         Ok(db) => add_reminder_to_collection(db.collection::<Reminder>("Reminders"), reminder).await,
         Err(err) => {
             eprintln!("Error: something went wrong when trying to add a reminder to the DB: {}", err);
         }
     }
+
 }
 
 pub fn register() -> CreateCommand {
@@ -126,15 +131,15 @@ pub fn check_for_reminders() {
 }
 
 async fn add_reminder_to_collection(collection: Collection<Reminder>, reminder:  Reminder) {
-    collection.insert_one(reminder, None).await.ok();
+   collection.insert_one(reminder, None).await.ok();
 }
 
-fn get_millisecond_conversion_factor(unit_from_user: TimeUnit) -> i128 {
+fn get_second_conversion_factor(unit_from_user: TimeUnit) -> i64 {
     return match unit_from_user {
-        TimeUnit::Minutes => 60000,
-        TimeUnit::Hours => 60000 * 60,
-        TimeUnit::Days => 60000 * 60 * 24,
-        TimeUnit::Months => 60000 * 60 * 30, // Using a avg here
-        TimeUnit::Years => 60000 * 60 * 365,
+        TimeUnit::Minutes => 60,
+        TimeUnit::Hours => 60 * 60,
+        TimeUnit::Days => 60 * 60 * 24,
+        TimeUnit::Months => 60 * 60 * 30, // Using a avg here
+        TimeUnit::Years => 60 * 60 * 365,
     }
 }
