eb0044cad8698656103b4e6568c7b567f215768d
Vexrax <joshuaf_111@live.com>
2025-01-19 12:27:25 -0500

    Formatting

--- src/commands/messageleaderboard.rs
+++ src/commands/messageleaderboard.rs
@@ -43,7 +43,9 @@ pub fn register() -> CreateCommand {
 }
 
 pub fn build_embed(message_counts_by_user: HashMap<User, u32>, message_counts_by_channel_by_user: HashMap<String, HashMap<User, u32>>) -> CreateEmbed {
-    let mut message_counts_by_channel_sorted: Vec<_> = get_messages_by_channel(message_counts_by_channel_by_user).into_iter().collect();
+
+    let mut message_counts_by_channel =  get_messages_by_channel(message_counts_by_channel_by_user);
+    let mut message_counts_by_channel_sorted: Vec<_> = message_counts_by_channel.into_iter().collect();
     message_counts_by_channel_sorted.sort_by(|a, b| b.1.cmp(&a.1));
 
     let mut message_counts_by_user_sorted: Vec<_> = message_counts_by_user.into_iter().collect();
@@ -82,7 +84,7 @@ pub fn build_embed(message_counts_by_user: HashMap<User, u32>, message_counts_by
         .color(Color::TEAL)
         .fields(fields.into_iter())
         .thumbnail("https://tr.rbxcdn.com/04c6e20f26515ddbcbc5adaf78ce6f09/420/420/Hat/Png")
-        .footer(CreateEmbedFooter::new(&format!("{} messages in {} channels", 0, 0)));
+        .footer(CreateEmbedFooter::new(&format!("{} messages in {} channels", message_counts_by_channel.values().sum(), message_counts_by_channel.len())));
 }
 
 fn get_messages_by_channel(message_counts_by_channel: HashMap<String, HashMap<User, u32>>) -> HashMap<String, u32>{
